f51e012209d2dcad60bd6d3c3819face8beb2e92
VickyMutai <vicky.mutai96@gmail.com>
2018-02-09 18:54:01 +0300

    add template to view profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required
 
@@ -10,3 +10,12 @@ def index():
     '''
     title = 'VICTORIA MUTAI'
     return render_template('index.html',title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user=User.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html",user=user)
