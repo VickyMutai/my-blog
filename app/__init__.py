4a937acf8cde104825fcf732175b8b6e3b1aeaa6
VickyMutai <vicky.mutai96@gmail.com>
2018-02-09 11:41:58 +0300

    setup project structure

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+def create_app(config_name):
+    app = Flask(__name__)
+    #creating app configurations
+    app.config.from_object(config_options[config_name])
+
+    #initializing flask extensions
+    bootstrap.init_app(app)
+
+    #registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    #will add views and forms
+
+    return app
---
+++ start.sh
@@ -0,0 +1,3 @@
+export SECRET_KEY='213219498741721'
+
+python3.6 manage.py server
