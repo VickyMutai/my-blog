1f47ff8bfbc5a5a60a3eb211ac35550027b51440
VickyMutai <vicky.mutai96@gmail.com>
2018-02-09 18:08:09 +0300

    create template for registration

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,22 +4,23 @@ from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
 
-class RegistrationForm():
+class RegistrationForm(FlaskForm):
     email = StringField('Enter your Email',validators=[Required(),Email()])
-    def validate_email(self,data_field):
-        if User.query.filter_by(email=data_field.data).first():
-            raise ValidationError('There is an account with that email')
-    name = StringField('Full Name: ',validator=[Required()])
+    name = StringField('Full Name: ',validators=[Required()])
     username = StringField('User Name: ',validators=[Required()])
-    def validate_username(self,data_field):
-        if User.query.filter_by(username=data_field.data).first():
-            raise ValidationError('That username is taken')
     password = PasswordField('Password', validators=[Required(),
     EqualTo('password_confirm',message='Passwords must match')])
     password_confirm = PasswordField('Confirm Password',validators=[Required()])
     submit = SubmitField('Sign Up')
 
-class LoginForm():
+    def validate_email(self,data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError('There is an account with that email')
+    def validate_username(self,data_field):
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError('That username is taken')
+
+class LoginForm(FlaskForm):
     email = StringField('Enter your Email',validators=[Required(),Email()])
     password = PasswordField('Password',validators=[Required()])
     remember = BooleanField('Remember Me')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
-from flask import render_template
+from flask import render_template,request,url_for,redirect,flash
 from . import auth
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import LoginForm,RegistrationForm
+from .. import db
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
