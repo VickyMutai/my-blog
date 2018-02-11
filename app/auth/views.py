021ae95627966f89effdbac0e76c5812f4f45df5
VickyMutai <vicky.mutai96@gmail.com>
2018-02-11 17:03:20 +0300

    Enable user to edit  their profile

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,20 @@
-from flask import render_template,request,url_for,redirect,flash
-from . import auth
+from flask import render_template,redirect,url_for,request,flash
 from flask_login import login_user,logout_user,login_required
+from . import auth
 from ..models import User
-from .forms import LoginForm,RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from .. import db
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email= form.email.data, username=form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -18,18 +29,6 @@ def login():
 
     title = "Pitch login"
     return render_template('auth/login.html',login_form = login_form,title=title)
-
-@auth.route('/register', methods=["GET","POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data,name = form.name.data,username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form=form)
-
 
 @auth.route('/logout')
 @login_required
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 @login_manager.user_loader
@@ -10,12 +11,13 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255), index=True)
     name = db.Column(db.String(255))
     email = db.Column(db.String(255),unique=True,index=True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
+
     
     @property
     def password(self):
@@ -23,10 +25,10 @@ class User(db.Model, UserMixin):
 
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return f'{self.username}'
