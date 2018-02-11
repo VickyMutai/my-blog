204867e457cf33bc5d5be48d3150128e242e84ed
VickyMutai <vicky.mutai96@gmail.com>
2018-02-11 17:36:48 +0300

    configure flask uploads variables

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://vicky:0716889165@localhost/blog'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+    UPLOADED_PHOTOS_DEST='app/static/photos'
 
 class ProdConfig(Config):
     pass
