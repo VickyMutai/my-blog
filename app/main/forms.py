e87d006330899a23250543c6b490d1ef1fe304a6
VickyMutai <vicky.mutai96@gmail.com>
2018-02-11 19:23:46 +0300

    create for blogs

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,3 +5,7 @@ from wtforms.validators import Required
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about yourself', validators=[Required()])
     submit = SubmitField('Submit')
+
+class PostForm(FlaskForm):
+    body = TextAreaField("Whats on your mind?", validators=[Required()])
+    submit = SubmitField('Submit')
